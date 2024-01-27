4fa85676279c307c200e6eceb72cb43f5593c13d
ming wei <mweimwei@yahoo.com>
2024-01-27 17:22:54 -0500

    add log

--- compare_sentence.py
+++ compare_sentence.py
@@ -2,6 +2,14 @@ from sentence_transformers import SentenceTransformer, util
 import torch
 import csv
 import io 
+import logging
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
 
 csvfile = ".\\mynote1.csv"
 
